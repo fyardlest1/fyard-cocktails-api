edff5d8bf5b12a941b9928735ac84b5e1724a3d5
fyardlest <fyardlest1@gmail.com>
2021-01-25 19:38:32 -0500

    Search Form OK

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import presetDrinks from './src/presentDrinks.js'
+import './src/searchForm.js'
+
 const URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a"
 
 window.addEventListener('DOMContentLoaded', () => {
