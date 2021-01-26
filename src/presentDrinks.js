7e40bd891d8c5f951bbb272fda67015f8198724d
fyardlest <fyardlest1@gmail.com>
2021-01-25 19:08:40 -0500

    Display Drinks

--- src/presentDrinks.js
+++ src/presentDrinks.js
@@ -1,9 +1,12 @@
 import fetchDrinks from './fetchDrinks.js';
+import displayDrinks from './displayDrinks.js'
 
 const showDrinks = async (URL) => {
     const data = await fetchDrinks(URL)
+
     console.log(data)
     // fetch drinks
+    const section = await displayDrinks(data)
     // display drinks
 }
 
